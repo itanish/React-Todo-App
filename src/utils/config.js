0caff867b508c1312ca0508f746b5951b08c745a
itanish <tanishsehgal1234@gmail.com>
2020-04-25 05:38:33 +0530

    Upload on Heroku

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 export default {
   AUTH0_CLIENT_ID: 'pHgkDWOo36FcQICvfFZZUQi6y8a0HvVO',
   AUTH0_DOMAIN: 'dev-sdutv2bs.auth0.com',
-  REDIRECT_URL: 'http://localhost:3000/callback'
+  REDIRECT_URL: 'https://my-todolist-react.herokuapp.com/callback'
 };
